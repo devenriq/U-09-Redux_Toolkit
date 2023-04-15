66953cfd68a902fcb6634d9b6b455ce407c53017
enrique palomino <octepc.97@gmail.com>
2023-04-15 17:34:15 -0500

    feat: :sparkles: Working with the RTK tool to consume API's

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { useGetTodosQuery, useGetTodoQuery } from "./store/apis/todosApi";
+
 export const TodoApp = () => {
+  const [todoId, setTodoId] = useState(1);
+
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo = [], isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+
+    setTodoId(todoId - 1);
+  };
+
+  console.log(todo);
+
   return (
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
-      <pre>...</pre>
+      <h4>isLoading: {isLoading ? "true" : "false"} </h4>
+      <pre>{JSON.stringify(todo)}</pre>
 
-      <button>Next Todo</button>
+      <button onClick={prevTodo}>Prev Todo</button>
+      <button onClick={nextTodo}>Next Todo</button>
+
+      {/* <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.completed ? "Complete" : "Pending"}</strong>
+            {todo.title}
+          </li>
+        ))}
+      </ul> */}
     </>
   );
 };
--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -1,17 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const todosApi = createApi({
+  //Nombre de la función
   reducerPath: "todos",
 
+  //Path estático de donde sacará la información
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com",
   }),
 
+  //Función asíncrona que sirve para obtener la información de determinado endpoint
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
     }),
+    getTodo: builder.query({
+      query: (todoId) => `/todos/${todoId}`,
+    }),
   }),
 });
 
-export const { useGetTodosQuery } = todosApi;
+//Se exporta un hook personalizado. En este caso, se le ha puesto el nombre de useGetTodosQuery,
+//El hook por defecto aporta cuatro elementos: isLoading, isError, data y error. En data se encuentra toda la información del API
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
